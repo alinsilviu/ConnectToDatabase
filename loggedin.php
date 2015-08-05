d010a2532b97f5d5ad46a2cad6be284fb434f8ce
Alin Palici <silviualin067@gmail.com>
2015-08-05 21:44:18 +0300

    Connect to my Database

---
+++ loggedin.php
@@ -0,0 +1,12 @@
+<?php
+
+if (!isset($_COOKIE['user_id'])) {
+	require ('login_functions.inc.php');
+	redirect_user();
+}
+$page_title = 'Logged In!';
+include ('includes/header.html');
+echo "<h1>Logged In!</h1>You are now logged in, {$_COOKIE['first_name']}!</p><p><a href=\"logout.php\">Logout</a></p>";
+
+include ('includes/footer.html');
+?>
--- view_users.php
+++ view_users.php
@@ -48,47 +48,47 @@ switch ($sort) {
 $q = "SELECT last_name, first_name, DATE_FORMAT(registration_date, '%M %d, %Y') AS dr, user_id FROM users ORDER BY $order_by LIMIT $start, $display";
 $r = @mysqli_query ($dbc, $q);
 
-	echo '<table align="center" cellspacing="0" cellpadding="5" width="75%">
-		<tr>
-			<td align="left"><b>Edit</b></td>
-			<td align="left"><b>Delete</b></td>
-			<td align="left"><b><a href="view_users.php?sort=ln">Last Name</a></b></td>
-			<td align="left"><b><a href="view_users.php?sort=fn">First Name</a></b></td>
-			<td align="left"><b><a href="view_users.php?sort=rd">Date Registered</a></b></td>
+echo '<table align="center" cellspacing="0" cellpadding="5" width="75%">
+	<tr>
+		<td align="left"><b>Edit</b></td>
+		<td align="left"><b>Delete</b></td>
+		<td align="left"><b><a href="view_users.php?sort=ln">Last Name</a></b></td>
+		<td align="left"><b><a href="view_users.php?sort=fn">First Name</a></b></td>
+		<td align="left"><b><a href="view_users.php?sort=rd">Date Registered</a></b></td>
+	</tr>';
+$bg = '#eeeeee';
+while ($row = mysqli_fetch_array($r, MYSQLI_ASSOC)) {
+	$bg = ($bg=='#eeeeee' ? '#ffffff' : '#eeeeee');
+	echo '<tr bgcolor="' . $bg . '">
+		<td align="left"><a href="edit_user.php?id=' . $row['user_id'] . '">Edit</a></td>
+		<td align="left"><a href="delete_user.php?id=' . $row['user_id'] . '">Delete</a></td>
+		<td align="left">' . $row['last_name'] . '</td>
+		<td align="left">' . $row['first_name'] . '</td>
+		<td align="left">' . $row['dr'] . '</td>
 		</tr>';
-	$bg = '#eeeeee';
-	while ($row = mysqli_fetch_array($r, MYSQLI_ASSOC)) {
-		$bg = ($bg=='#eeeeee' ? '#ffffff' : '#eeeeee');
-		echo '<tr bgcolor="' . $bg . '">
-			<td align="left"><a href="edit_user.php?id=' . $row['user_id'] . '">Edit</a></td>
-			<td align="left"><a href="delete_user.php?id=' . $row['user_id'] . '">Delete</a></td>
-			<td align="left">' . $row['last_name'] . '</td>
-			<td align="left">' . $row['first_name'] . '</td>
-			<td align="left">' . $row['dr'] . '</td>
-			</tr>';
-	}
-	echo '</table>';
-	mysqli_free_result ($r);
-	mysqli_close($dbc);
+}
+echo '</table>';
+mysqli_free_result ($r);
+mysqli_close($dbc);
 
-	if ($pages > 1) {
-		echo '<br /><p>';
-		$current_page = ($start/$display) + 1;
-		if ($current_page != 1) {
-			echo '<a href="view_users.php?s=' . ($start - $display) . '&p=' . $pages . '&sort=' . $sort . '">Previous</a> ';
-		}
-		for ($i = 1; $i <= $pages; $i++) {
-			if ($i != $current_page) {
-				echo '<a href="view_users.php?s=' . (($display * ($i - 1))) . '&p=' . $pages . '&sort=' . $sort . '">' . $i . '</a> ';
-			} else {
-				echo $i . ' ';
-			}
-		}
-		if ($current_page != $pages) {
-			echo '<a href="view_users.php?s=' . ($start + $display) . '&p=' . $pages . '&sort=' . $sort . '">Next</a>';
-		}
-		echo '</p>';
+if ($pages > 1) {
+	echo '<br /><p>';
+	$current_page = ($start/$display) + 1;
+	if ($current_page != 1) {
+		echo '<a href="view_users.php?s=' . ($start - $display) . '&p=' . $pages . '&sort=' . $sort . '">Previous</a> ';
 	}
-	include ('includes/footer.html');
+	for ($i = 1; $i <= $pages; $i++) {
+		if ($i != $current_page) {
+			echo '<a href="view_users.php?s=' . (($display * ($i - 1))) . '&p=' . $pages . '&sort=' . $sort . '">' . $i . '</a> ';
+		} else {
+			echo $i . ' ';
+		}
+	}
+	if ($current_page != $pages) {
+		echo '<a href="view_users.php?s=' . ($start + $display) . '&p=' . $pages . '&sort=' . $sort . '">Next</a>';
+	}
+	echo '</p>';
+}
+include ('includes/footer.html');
 
 ?>
